46ec83666099793a0bd9cb58d01c68b4c81c2681
Muhammad Ashhad Safwan <m_ashhadhlr@yahoo.com>
2023-04-30 16:45:45 +0500

    AppBar creation - v1.0.1.1

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,24 @@ class MyApp extends StatelessWidget {
       title: "Lottery App",
       home: Scaffold(
       appBar: AppBar(
-        title: Text("Lottery App", style: TextStyle(color: Colors.amberAccent)),
-        backgroundColor: Colors.black38,
+        title: Text("Lottery App", style: TextStyle(color: Colors.yellow[600])),
+        backgroundColor: Colors.indigo[900],
       ),
       backgroundColor: Colors.white70,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text('Welcome Muhammad', style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.black
+              ),),
+            )
+          ],
+        ),
+      ),
+
       // home: AppBar(
       //   title: Text("Lottery App", style: TextStyle(color: Colors.amberAccent)),
       //   backgroundColor: Colors.black38,
