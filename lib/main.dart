73b2c55c6a44e6809ddd96658ead701650a88111
Muhammad Ashhad Safwan <m_ashhadhlr@yahoo.com>
2023-04-30 17:22:43 +0500

    AppBar creation - v1.0.1.2

--- lib/main.dart
+++ lib/main.dart
@@ -2,42 +2,62 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  int x = 0;
+  @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Lottery App",
       home: Scaffold(
-      appBar: AppBar(
-        title: Text("Lottery App", style: TextStyle(color: Colors.yellow[600])),
-        backgroundColor: Colors.indigo[900],
-      ),
-      backgroundColor: Colors.white70,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text('Welcome Muhammad', style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.black
-              ),),
-            )
-          ],
+        appBar: AppBar(
+          title: Text("Lottery App", style: TextStyle(color: Colors.yellow[600])),
+          backgroundColor: Colors.indigo[900],
         ),
-      ),
+        backgroundColor: Colors.white70,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text('You pressed the floating button:', style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.black
+                ),),
+              ),
+              Text(x.toString(), style:TextStyle(fontSize: 16)),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            x++;
+            setState(() {
 
-      // home: AppBar(
-      //   title: Text("Lottery App", style: TextStyle(color: Colors.amberAccent)),
-      //   backgroundColor: Colors.black38,
+            });
+            print(x.toString());
+          },
+          child: Icon(Icons.add_circle),
+        ),
+
+        // home: AppBar(
+        //   title: Text("Lottery App", style: TextStyle(color: Colors.amberAccent)),
+        //   backgroundColor: Colors.black38,
       ),
 
     );
   }
 }
+
