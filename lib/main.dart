102528fec5b0135c25e83503a70e51bdb2e09cc3
Muhammad Ashhad Safwan <m_ashhadhlr@yahoo.com>
2023-04-30 18:07:56 +0500

    Updated AppBar - v1.0.1.2

--- lib/main.dart
+++ lib/main.dart
@@ -13,51 +13,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-  int x = 0;
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Lottery App",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Lottery App", style: TextStyle(color: Colors.yellow[600])),
-          backgroundColor: Colors.indigo[900],
+          title: Text("Lottery App"),
+          centerTitle: true,
         ),
-        backgroundColor: Colors.white70,
-        body: SafeArea(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text('You pressed the floating button:', style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.black
-                ),),
-              ),
-              Text(x.toString(), style:TextStyle(fontSize: 16)),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            x++;
-            setState(() {
+        body: Column(
+          children: [
 
-            });
-            print(x.toString());
-          },
-          child: Icon(Icons.add_circle),
+          ],
         ),
 
-        // home: AppBar(
-        //   title: Text("Lottery App", style: TextStyle(color: Colors.amberAccent)),
-        //   backgroundColor: Colors.black38,
       ),
-
     );
   }
 }
-
